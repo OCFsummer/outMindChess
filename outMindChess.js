35644bcc744676d87d483d958636233d23647e59
zxxx <zxxx@willshare.cn>
2018-04-20 18:12:54 +0800

    重构Controllboard函数，重构Chess函数，优化游戏

--- outMindChess.js
+++ outMindChess.js
@@ -1,18 +1,3 @@
-var attacker = undefined;
-var attackTarget = undefined;
-var currentCamp = 1;
-var camps = {
-	1:{
-		attacknum:1,
-		movenum:1
-	},
-	2:{
-		attacknum:1,
-		movenum:1
-	}
-}
-
-
 function Chess(obj){
 	this._chessName = obj.chessName;
 	this._chessImg = obj.chessImg;
@@ -21,8 +6,6 @@ function Chess(obj){
 	this._damageColorNodes = [];
 	this._skill = obj.skill;
 	this._startLocation = obj.startLocation;
-	this._checkerBoard = obj.checkerBoard;
-	this._moveColorBoard = obj.moveColorBoard;
 	this._grid = obj.grid;
 
 	this._hero = obj.hero;
@@ -38,7 +21,7 @@ Chess.prototype = {
 	},
 	creatChess:function(){
 		var _this = this;
-		this._chess = $("<img src='./imgs/"+_this._chessImg+".png' class='heroicon' draggable='true' id='"+_this._chessName+"'>");
+		this._chess = $("<img src='./imgs/heros/"+_this._chessImg+".png' class='heroicon' draggable='true' id='"+_this._chessName+"'>");
 	},
 	moveInit:function(){
 		var _this = this;
@@ -49,6 +32,7 @@ Chess.prototype = {
 	heroInit:function(){
 		var _this = this;
 		this.getMoveRangeNodes();
+		this.initSkills();
 	},
 	/*----基本移动逻辑模块----*/
 	putChess:function(Coordinate){
@@ -63,7 +47,7 @@ Chess.prototype = {
 		var neighbors = help.getRound(_this._hero.moveRange, parent);
 		for (var i = 0; i < neighbors.length; i++) {
 			var index = help.getIndex(neighbors[i]);
-			var node = this._checkerBoard.children().eq(index);
+			var node = checkerBoard.children().eq(index);
 			this.bindGridDrop(node);
 		}
 		this.bindChessOutEvent();
@@ -89,27 +73,28 @@ Chess.prototype = {
 		node.on('drop.'+_this._chessName, function(e){
 			e.preventDefault();
 			var data=e.originalEvent.dataTransfer.getData('chessName');
+			var attacker = controllboard.getAttacker();
+
 			_this.showMoveRoundColor($('#'+data), 3);
 			if(data == _this._chessName && this.children.length == 0){
 				$(e.target).append(document.getElementById(data));
 				_this.showMoveRoundColor($('#'+data), 2);
-				camps[_this.getCamp()]['movenum'] = 0;
-				/*触发click*/
-				_this._chess.click();
+				controllboard.setActionPoint(attacker.getCamp(), 'movenum', 0)
+				//触发click
 			}
 		});
 	},
 	bindDragstart:function(){
-		var _this = this;
+/*		var _this = this;
 		this._chess.on('dragstart',function(e){
 			//判断是否为该轮阵营的玩家
 			if(_this.getCamp() == currentCamp){
 				if(camps[_this.getCamp()]['movenum'] > 0){
-					if($('#controlboard').length > 0){
+					if($('#controllboard').length > 0){
 						attacker.showDamageRoundColor(2);
 						attackTarget = undefined;
 						attacker = undefined;
-						$('#controlboard').remove();
+						$('#controllboard').remove();
 					}
 					e.originalEvent.dataTransfer.setData('chessName',e.target.id);
 					_this.showMoveRoundColor($(e.target), 1);
@@ -122,24 +107,57 @@ Chess.prototype = {
 				$('.bordertips').text('该轮是camp为'+currentCamp+'的玩家');
 			}
 			popHeroInfo(false);
-		});
-	},
-	showDamageRoundColor:function(type){
+		});*/
 		var _this = this;
-		if(type == 1){
-			var damageRangeNodes = this.getDamageRangeNodes();
+		this._chess.on('dragstart',function(e){
+			popHeroInfo();
+			help.writeBordertips();
+			controllboard.writeAttacktips();
+			var attackInfo = controllboard.judgeHero(_this);
+			switch(attackInfo.type){
+				case 'attacker':
+					var attacker = controllboard.getAttacker();
+					if(controllboard.getActionPoint(attacker.getCamp()).movenum > 0){
+						attacker.showMoveRoundColor($(e.target), 1);
+						controllboard.writeController(_this._chessName);
+						e.originalEvent.dataTransfer.setData('chessName',e.target.id);
+					}
+					else{
+						controllboard.writeAttacktips('移动次数为0，无法移动');
+					}
+					break;
+				case 'changeAttacker':
+					var attacker = controllboard.getAttacker();
+					if(controllboard.getActionPoint(attacker.getCamp()).movenum > 0){
+						attacker.showMoveRoundColor($(e.target), 1);
+						controllboard.writeController(_this._chessName);
+						e.originalEvent.dataTransfer.setData('chessName',e.target.id);
+					}
+					else{
+						controllboard.writeAttacktips('移动次数为0，无法移动');
+					}
+					break;
+				default:
+					help.writeBordertips("不是你的回合 请阵营为："+controllboard.currentCamp()+"开始操作");
+					break;			
+			}
+		})
+	},
+	showDamageRoundColor:function(hero, show){
+		if(show == true){
+			var damageRangeNodes = hero.getDamageRangeNodes();
 			for (var i = 0; i < damageRangeNodes.length; i++) {
 				var index = help.getIndex(damageRangeNodes[i]);
-				var node = this._moveColorBoard.children().eq(index);
-				this._damageColorNodes.push(node);
+				var node = moveColorBoard.children().eq(index);
+				hero._damageColorNodes.push(node);
 				node.css({
 					'background-color':'red'
 				});
 			}
 		}
-		else if(type == 2){
-			for (var i = 0; i < this._damageColorNodes.length; i++) {
-				this._damageColorNodes[i].css({
+		else{
+			for (var i = 0; i < hero._damageColorNodes.length; i++) {
+				hero._damageColorNodes[i].css({
 					'background-color':''
 				})
 			}
@@ -154,7 +172,7 @@ Chess.prototype = {
 			this._moveColorNodes = [];
 			for (var i = 0; i < neighbors.length; i++) {
 				var index = help.getIndex(neighbors[i]);
-				var node = this._moveColorBoard.children().eq(index);
+				var node = moveColorBoard.children().eq(index);
 				this._moveColorNodes.push(node);
 				node.css({
 					'background-color':'green'
@@ -165,7 +183,7 @@ Chess.prototype = {
 			this._grid.off('drop.'+this._chessName);
 			for (var i = 0; i < neighbors.length; i++) {
 				var index = help.getIndex(neighbors[i]);
-				var node = this._checkerBoard.children().eq(index);
+				var node = checkerBoard.children().eq(index);
 				this.bindGridDrop(node);
 			}
 			this._moveColorNodes = [];
@@ -184,34 +202,26 @@ Chess.prototype = {
 	iconClick:function(){
 		var _this = this;
 		this._chess.on('click.controllboard', function(e){
+			help.writeBordertips();
+			controllboard.writeAttacktips();
 			//判断是否为攻击发起人
-			if(attacker == undefined){
-				if(_this.getCamp() == currentCamp){
-					_this.showDamageRoundColor(1);
-					controllboard('attack', 'attacker', _this);
-					popHeroInfo(false);
-				}
-				else{
-					$('.bordertips').text('该轮是camp为'+currentCamp+'的玩家');
-				}
-			}
-			else{
-				//判断切换攻击人还是对目标进行攻击
-				if(attacker.getCamp() == _this.getCamp()){
-					if(_this.getCamp() == currentCamp){
-						attacker.showDamageRoundColor(2);
-						_this.showDamageRoundColor(1);
-						controllboard('attack', 'attacker', _this);
-						popHeroInfo(false);
+			var attackInfo = controllboard.judgeHero(_this);
+			switch(attackInfo.type){
+				case 'attacker':
+					controllboard.writeController(_this._chessName);
+					break;
+				case 'changeAttacker':
+					controllboard.writeController(_this._chessName);
+					break;
+				case 'attackTarget':
+					if(attackInfo.info == 'normalAttack'){
+						basicCount.attack(controllboard.getAttacker(), controllboard.getAttackTarget());
 					}
-					else{
-						$('.bordertips').text('该轮是camp为'+currentCamp+'的玩家');
+					break;
+				case false:
+					if(attackInfo.info == 'camp'){
+						help.writeBordertips("不是你的回合 请阵营为："+controllboard.currentCamp()+"开始操作");
 					}
-				}
-				else{
-					controllboard('attack', 'attackTarget', _this);
-					popHeroInfo(true, _this._hero, e);
-				}
 			}
 		})
 	},
@@ -233,7 +243,14 @@ Chess.prototype = {
 		var location = help.getCoordinate(parent);
 		return location;
 	},
+	/*----技能相关模块----*/
+	initSkills:function(){
+		return this._hero.skills;
+	},
 	/*----人物属性模块----*/
+	getDom:function(){
+		return this._chess;
+	},
 	getBlood:function(){
 		return this._hero.blood;
 	},
@@ -280,18 +297,47 @@ Chess.prototype = {
 	},
 	setDamageRange:function(arg){
 		return this._hero.damageRange = arg;
-	},
-	/*----技能逻辑模块----*/
-	setSkill:function(){
-
 	}
+
 }
 /*----------------技能函数------------------*/
 function SkillPool(){
 
 }
 SkillPool.prototype = {
-
+	/*-技能辅助函数-*/
+	blinblinDom:function(moveRanges){
+		for (var i = 0; i < moveRanges.length; i++) {
+			bl = attacker.getLocation().toString();
+			if(moveRanges[i].toString() == bl){
+				continue;
+			}
+			var index = help.getIndex(moveRanges[i]);
+			var node = checkerBoard.children().eq(index);
+			node.css({
+				'opacity':0,
+				'background-color':'blue'
+			});
+			(function(node){
+				node.animate({
+					'opacity':1
+				}, function(){
+					node.animate({'opacity':0},function(){
+						node.css({
+							'background-color':'',
+							'opacity':1
+						})
+					})
+				});
+			})(node)
+		}
+	},
+	/*-技能函数-*/
+	starStorm:function(attacker){
+		var dom = attacker.getDom();
+		var moveRanges = help.getRound(1, dom.parent());
+		this.blinblinDom(moveRanges);
+	}
 }
 /*----------------功能函数------------------*/
 /*border创建，坐标计算等通用函数*/
@@ -356,6 +402,14 @@ GetLocation.prototype = {
 			}
 		}
 		return arr;
+	},
+	writeBordertips:function(info){
+		var dom = $('.bordertips');
+		if(info == undefined){
+			dom.text("");
+			return;
+		}
+		dom.text(info);
 	}
 
 }
@@ -364,7 +418,7 @@ GetLocation.prototype = {
 function BasicCount(){
 }
 BasicCount.prototype = {
-	basicAttack:function(attacker, attackTarget){
+	attackCount:function(attacker, attackTarget){
 		var damage = this.countbasicDamage(attacker.getDamage(), attackTarget.getArmor());
 		var blood = attackTarget.setBlood(attackTarget.getBlood() - damage);
 		if(blood > 0 ){
@@ -390,6 +444,23 @@ BasicCount.prototype = {
 			if(a[i].toString() == bl){
 				return true;
 			}
+		}
+	},
+	attack:function(attacker, attackTarget){
+		if(controllboard.getActionPoint(attacker.getCamp()).attacknum > 0){
+			controllboard.setAttackTarget(attackTarget);
+			var range = this.attackRangeCount(attacker, attackTarget);
+			if(range){
+				var attackfinish = this.attackCount(attacker, attackTarget);
+				controllboard.getActionPoint(attacker.getCamp()).attacknum = 0;
+				controllboard.writeAttacktips('攻击完成');
+			}
+			else{
+				controllboard.writeAttacktips('目标太远，无法攻击');
+			}
+		}
+		else{
+			controllboard.writeAttacktips('攻击次数为0，无法攻击');
 		}
 	}
 }
@@ -417,100 +488,235 @@ function popHeroInfo(creat, obj, e){
 }
 
 /*----------------控制面板函数------------------*/
-function controllboard(action, type, hero){
-	function AttackEvent(type,hero){
-		switch(type){
-			case 'attacker':
-				/*创建面板*/
-				var board = $("<div id='controlboard'></div>");
-				var attackButton = $("<div class='controllboard_attackbutton controllboard_button'>攻击</div>");
-				var attackTips = $("<div class='attacktips'></div>");
-				var finish = $("<div class='finish controllboard_button'>完成行动</div>");
-				board.append(attackButton);
-				board.append(attackTips);
-				board.append(finish);
-				$('#wrap').append(board);
-				/*业务逻辑*/
-				attacker = hero;
-				attackButton.on('click', function(){
-					attackTips.text('请指定攻击目标');
-				})
-				finish.on('click', function(){
-					attacker.showDamageRoundColor(2);
-					camps[attacker.getCamp()]['attacknum'] = 1;
-					camps[attacker.getCamp()]['movenum'] = 1;
-					attackTarget = undefined;
-					attacker = undefined;
-					currentCamp == 1?currentCamp = 2:currentCamp = 1;
-					$('.bordertips').text("");
-					$('#controlboard').remove();
-				})
-				break;
-			case 'attackTarget':
-				if(camps[attacker.getCamp()]['attacknum'] > 0){
-					attackTarget = hero;
-					var range = basicCount.attackRangeCount(attacker, attackTarget);
-					if(range){
-						var attackfinish = basicCount.basicAttack(attacker, attackTarget);
-						camps[attacker.getCamp()]['attacknum'] = 0;
-						$('.attacktips').text('攻击完成');
-					}
-					else{
-						$('.attacktips').text('目标太远，无法攻击');
-					}
-				}
-				else{
-					$('.bordertips').text("攻击次数为0");
-				}
-				break;
-
+function Controllboard(){
+	this._attacker = undefined;
+	this._attackTarget = undefined;
+	this._attackType = {
+		info:undefined,
+		callback:undefined
+	};
+	this._currentCamp = 1;
+	this._camps = {
+		1:{
+			attacknum:1,
+			movenum:1
+		},
+		2:{
+			attacknum:1,
+			movenum:1
 		}
-	}
-	if(action == 'attack'){
-		$('.bordertips').text("");
-		AttackEvent(type,hero);
-	}
+	};
+	this.init();
+}
+
+Controllboard.prototype = {
+	init:function(){
+		var board = $("<div id='controllboard'></div>");
+		var controller = $("<div class='controllerTip'>选择英雄：无</div>")
+		var attackButton = $("<div class='controllboard_attackbutton controllboard_button'>攻击</div>");
+		var attackTips = $("<div class='attacktips'></div>");
+		var finish = $("<div class='finish controllboard_button'>完成行动</div>");
+		board.append(controller);
+		board.append(attackButton);
+		board.append(attackTips);
+		board.append(finish);
+		$('#wrap').append(board);
+		this.eventCreat();
+	},
+	eventCreat:function(){
+		this.attackButtonClick();
+		this.finishButtonClick();
+	},
+	finishButtonClick:function(){
+		var _this = this;
+		$('.finish').on('click', function(){
+			if(_this._attacker){
+				_this._attacker.showDamageRoundColor(_this._attacker, false);
+				_this.getActionPoint(_this._attacker.getCamp()).attacknum = 1;
+				_this.getActionPoint(_this._attacker.getCamp()).movenum = 1;
+				_this._attackTarget = undefined;
+				_this._attacker = undefined;
+				_this._attackType = {
+					info:undefined,
+					callback:undefined
+				};
+				_this._camps = {
+					1:{
+						attacknum:1,
+						movenum:1
+					},
+					2:{
+						attacknum:1,
+						movenum:1
+					}
+				};
+			}
+			_this._currentCamp == 1?_this._currentCamp = 2:_this._currentCamp = 1;
+			_this.writeController();
+			_this.writeAttacktips();
+			help.writeBordertips();
+		})
+	},
+	attackButtonClick:function(){
+		var _this = this;
+		$('.controllboard_attackbutton').on('click', function(){
+			if(_this._attacker != undefined){
+				_this.setAttackType({info:'normalAttack',callback:undefined});
+				_this._attacker.showDamageRoundColor(_this._attacker, true);
+				_this.writeAttacktips('请指定攻击目标');
+			}
+			else{
+				_this.writeAttacktips('请选择英雄');
+			}
+		})
+
+	},
+	/*icon点击事件提供接口判断英雄角色*/
+	judgeHero:function(hero){
+		var _this = this;
+		if(this._attacker == undefined){
+			//进攻方，更新controllBoard
+			if(hero.getCamp() == this._currentCamp){
+				this._attacker = hero;
+				return {
+					type:'attacker'
+				}
+			}
+			//该回合不是当前hero阵营
+			return {
+					type:false,
+					info:'camp'
+				}
+		}
+		else{
+			if(this._attacker.getCamp() == hero.getCamp()){
+				//切换进攻人
+				this._attacker.showDamageRoundColor(_this._attacker, false);
+				this._attacker = hero;
+				this._attackType = {
+					info:undefined,
+					callback:undefined
+				};
+				return {
+					type:'changeAttacker'
+				}
+			}
+			else{
+				//被攻击方回调
+				var attackType = this.getAttackType();
+				this._attackTarget = hero;
+				switch(attackType.info){
+					case 'normalAttack':
+						return {
+							type:'attackTarget',
+							info:attackType.info
+						}
+						break;
+					default:
+						if(hero.getCamp() == this._currentCamp){
+							this.writeAttacktips('请选择攻击模式');
+						}
+						else{
+							return {
+								type:false,
+								info:'camp'
+							}
+						}
+						break;
+				}
+			}
+		}
+	},
+	judgeCurrentCamp:function(hero){
+		if(hero.getCamp() == this._currentCamp){
+			return true;
+		}
+	},
+	writeAttacktips:function(info){
+		var dom = $('.attacktips');
+		if(info == undefined){
+			dom.text("");
+			return;
+		}
+		dom.text(info);
+	},
+	writeController:function(info){
+		var dom = $('.controllerTip');
+		if(info == undefined){
+			dom.text("选择英雄：无");
+			return;
+		}
+		dom.text("选择英雄："+info);
+	},
+	currentCamp:function(){
+		return this._currentCamp;
+	},
+	getAttackType:function(){
+		return this._attackType;
+	},
+	getAttacker:function(){
+		return this._attacker;
+	},
+	getAttackTarget:function(){
+		return this._attackTarget;
+	},
+	getActionPoint:function(camp){
+		return this._camps[camp];
+	},
+	setAttacker:function(hero){
+		return this._attacker = hero;
+	},
+	setAttackTarget:function(hero){
+		return this._attackTarget = hero;
+	},
+	setAttackType:function(obj){
+		return this._attackType = obj;
+	},
+	setActionPoint:function(camp, type, num){
+		return this._camps[camp][type] = num;
+	},
 }
 
 
+var skillHelp = new SkillPool();
 var help = new GetLocation(40, 15);
+var controllboard = new Controllboard();
 var basicCount = new BasicCount();
+
+var checkerBoard = $("#Checkerboard");
+var moveColorBoard = $("#Movecolorboard");
 
 var obj1 = {
 	chessName:'撼地者',
 	chessImg:'撼地者',
 	startLocation:[0,0],
-	checkerBoard:$("#Checkerboard"),
-	moveColorBoard:$("#Movecolorboard"),
 	grid:$(".grid"),
 	hero:{
 		camp:1,
 		blood:100,
 		magic:50,
-		armor:5,
+		armor:7,
 		resistance:5,
-		moveRange:1,
+		moveRange:3,
 		damage:10,
-		damageRange:1,
+		damageRange:4,
 	}
 }
 
 var obj2 = {
 	chessName:'幽鬼',
 	chessImg:'幽鬼',
-	startLocation:[6,0],
-	checkerBoard:$("#Checkerboard"),
-	moveColorBoard:$("#Movecolorboard"),
+	startLocation:[7,0],
 	grid:$(".grid"),
 	hero:{
 		camp:1,
 		blood:100,
 		magic:50,
-		armor:5,
+		armor:7,
 		resistance:5,
 		moveRange:3,
 		damage:10,
-		damageRange:3,
+		damageRange:4,
 	}
 }
 
@@ -518,19 +724,20 @@ var obj3 = {
 	chessName:'米拉娜',
 	chessImg:'米拉娜',
 	camp:1,
-	startLocation:[11,0],
-	checkerBoard:$("#Checkerboard"),
-	moveColorBoard:$("#Movecolorboard"),
+	startLocation:[14,0],
 	grid:$(".grid"),
 	hero:{
 		camp:1,
 		blood:100,
 		magic:50,
-		armor:5,
+		armor:4,
 		resistance:5,
-		moveRange:2,
+		moveRange:3,
 		damage:10,
-		damageRange:2,
+		damageRange:4,
+		skills:{
+			starStorm:skillHelp.starStorm
+		}
 	}
 }
 
@@ -538,15 +745,13 @@ var obj4 = {
 	chessName:'敌法师',
 	chessImg:'敌法师',
 	camp:2,
-	startLocation:[0,11],
-	checkerBoard:$("#Checkerboard"),
-	moveColorBoard:$("#Movecolorboard"),
+	startLocation:[0,14],
 	grid:$(".grid"),
 	hero:{
 		camp:2,
 		blood:100,
 		magic:50,
-		armor:5,
+		armor:6,
 		resistance:5,
 		moveRange:2,
 		damage:10,
@@ -558,9 +763,7 @@ var obj5 = {
 	chessName:'冥界亚龙',
 	chessImg:'冥界亚龙',
 	camp:2,
-	startLocation:[6,11],
-	checkerBoard:$("#Checkerboard"),
-	moveColorBoard:$("#Movecolorboard"),
+	startLocation:[7,14],
 	grid:$(".grid"),
 	hero:{
 		camp:2,
@@ -578,15 +781,13 @@ var obj6 = {
 	chessName:'食人魔魔法师',
 	chessImg:'食人魔魔法师',
 	camp:2,
-	startLocation:[9,0],
-	checkerBoard:$("#Checkerboard"),
-	moveColorBoard:$("#Movecolorboard"),
+	startLocation:[10,0],
 	grid:$(".grid"),
 	hero:{
 		camp:2,
 		blood:100,
 		magic:50,
-		armor:5,
+		armor:4,
 		resistance:5,
 		moveRange:1,
 		damage:10,
